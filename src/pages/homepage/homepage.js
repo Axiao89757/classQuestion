32d12f2d4d113947ad7d3b4af0e99ed11dbb58e0
Leftscar <2213231357@qq.com>
2018-10-20 22:31:46 +0800

    homepage配置完成

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,84 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menu: [
+      {
+        name: '学生记录',
+        iconImageSrc: '/images/record.png'
+      },
+      {
+        name: '主动回答',
+        iconImageSrc: '/images/answer.png'
+      },
+      {
+        name: '随机抽问',
+        iconImageSrc: '/images/check.png'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function (e) {
+    var ct1 = wx.createCanvasContext('myCanvas')
+    ct1.moveTo(0, 0)
+    ct1.lineTo(1000,0)
+    ct1.setLineWidth(8)
+    ct1.stroke()
+    ct1.draw()
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
