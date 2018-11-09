c67f73058d6a9fdd3ee47a36a1232250069161fc
Leftscar <2213231357@qq.com>
2018-11-10 01:54:05 +0800

    添加班级1.0

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -17,10 +17,68 @@ Page({
   rmClass: function () {
 
   },
-  // 添加班级
-  addClass: function () {
 
+  /* 添加班级 */
+  // 点击添加按钮响应
+  addClass: function () {
+    this.setData({
+      hiddenInputClassMessages: false
+    })
   },
+  // 输入班级名响应
+  inputClassName: function (e) {
+    console.log(e.detail.value)
+    this.inputTempClassName = e.detail.value
+  },
+  // 输入学生总数响应
+  inputStudentNumber: function (e) {
+    this.inputTempStudentNumber = e.detail.value
+  },
+  // 信息填写确认响应
+  inputClassMessagesConfirm: function () {
+    var name = this.inputTempClassName
+    var num = this.inputTempStudentNumber
+    var obj = {className: name, studentNumber: '(' + num + '人)'} //把班级名和人数做成一个对象
+    // 判断输入是否合法
+    if (!name) {
+      wx.showModal({
+        content: '还没填写名字呢！',
+      })
+    } else if (!num) {
+      wx.showModal({
+        content: '还没填写人数呢！',
+      })
+    } else if (num < 1) {
+      wx.showModal({
+        content: '不可能只有0人吧？',
+      })
+    } else if (num > 300) {
+      wx.showModal({
+        content: '技术有限，目前一个班最多只能支持300人,带来不便还请谅解！',
+      })
+    } else {
+      this.data.classList.push(obj) // 用数组的push()函数将obj添加到对象数组中
+      var update = this.data.classList // 复制一份数组，以便后面的画面渲染赋值使用
+      this.setData({
+        classList: update
+      })
+      wx.setStorage({
+        key: 'classMessage',
+        data: this.data.classList
+      })
+      this.setData({
+        hiddenInputClassMessages: true
+      })
+    }
+    
+  },
+  // 信息填写取消响应
+  inputClassMessagesCancel: function () {
+    this.setData({
+      hiddenInputClassMessages: true
+    })
+  },
+
   // 选择班级
   classSelect: function () {
 
@@ -76,7 +134,7 @@ Page({
   },
 
 
-  // ‘随机抽查’响应
+  /*‘随机抽查’响应*/
   check: function (e) {
     var randomDigit = Math.floor(Math.random() * 93)
     wx.navigateTo({
@@ -84,12 +142,13 @@ Page({
     })
   },
 
-  // ‘学生记录’响应
+  /*‘学生记录’响应*/
   record: function (e) {
     wx.navigateTo({
       url: '/src/pages/record/record'
     })
   },
+
   /**
    * 页面的初始数据
    */
@@ -97,14 +156,16 @@ Page({
     currentClassSettingHiddent: true, // 班级管理弹窗隐藏控制变量
     currentClassName: '请选择班级',
 
+    hiddenInputClassMessages: true, // 输入班级信息弹窗控制
+    inputTempClassName: '', // 输入班级名暂时量
+    inputTempStudentNumber: 0, // 输入班级总人数暂时量
+
     hiddenmodalput: true, // 输入号码弹窗隐藏控制变量
     erroHiddenmodalput: true, // 输入号码错误弹窗隐藏控制变量
 
     inputNumber: '00',
 
-    classList: [
-      
-    ],
+    classList: [],
 
     menu: [
       {
@@ -129,20 +190,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    //每一次加载画面时，将获取数据缓存区的数据进行画面渲染
+    try {
+      var value = wx.getStorageSync('classMessage')
+      if (value) {
+        this.setData({
+          classList: value
+        })
+      }
+    } catch (e) {}
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function (e) {
-    var ct1 = wx.createCanvasContext('myCanvas')
-    ct1.moveTo(0, 0)
-    ct1.lineTo(1000,0)
-    ct1.setLineWidth(8)
-    ct1.stroke()
-    ct1.draw()
-
   },
 
   /**
